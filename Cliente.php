69870bf1715274a9190ab082a44e26c33ba466d5
Jaques Moura <jaquesmoura@gmail.com>
2017-06-19 22:09:40 -0400

    Primeiro Commit - Implementacao da Classe Cliente

---
+++ Cliente.php
@@ -0,0 +1,14 @@
+<?php
+
+class Cliente {
+	public $nome;
+	public cpf;
+	public endereco;
+
+	public function __construct($nome;$cpf;$endereco){
+		this->nome = $nome;
+		this->cpf = $cpf;
+		this->endereco = $endereco;
+	}
+
+}
